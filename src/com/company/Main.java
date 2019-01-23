3b8f6b8a424a9443928d76918684ca99372e447e
Jamez Stokes <jamezstokes@MacBook-Pro-6.local>
2019-01-23 11:49:30 -0500

    I finished getting the user input to go into the array.  I also finish getting the program to print out the average of all of the daily tempuratures

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,34 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
-//        Create a new project in IntelliJ IDEA named ArrayPractice
-//        Initialize your project as a Git repository and be sure to add and commit all changes as you work
-//        Declare an array named dailyAverageTemp
-//        Instruct the user to input daily average temperatures (in degrees Fahrenheit) for 7 days
-//        Each time the user inputs a value, add it to your dailyAverageTemp array
+//        Create a new project in IntelliJ IDEA named ArrayPractice:Done
+//        Initialize your project as a Git repository and be sure to add and commit all changes as you work:Done
+//        Declare an array named dailyAverageTemp: done
+//        Instruct the user to input daily average temperatures (in degrees Fahrenheit) for 7 days: done
+//        Each time the user inputs a value, add it to your dailyAverageTemp array: done
+
+        double average = 0;
+        double sum = 0;
+
+        Scanner scan = new Scanner(System.in);
+        double[] dailyAverageTemp = new double[7];
+
+        for (int i=0; i < dailyAverageTemp.length; i++ ) {
+            System.out.println("Enter a temperature number in degrees Fahrenheit for 7 day forecast.");
+            dailyAverageTemp[i] =scan.nextDouble();
+            sum = sum + dailyAverageTemp[i];
+        }
+
+        average = sum/7;
+        System.out.println(average);
+
+
 
     }
 }
